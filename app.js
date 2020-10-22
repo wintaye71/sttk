a185dcc493a119b558a4fb7d0934ae08580b1290
wintaye71 <ayewint71@gmail.com>
2020-10-22 08:26:30 +0630

    session

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const session = require('express-session')
 app.use(body_parser.json());
 app.use(body_parser.urlencoded());
 app.set('trust proxy', 1);
-app.use(session({secret: 'effystonem', resave: true, saveUninitialized: ture}));
+app.use(session({secret: 'effystonem', resave: true}));
 
 const bot_questions = {
   "q1": "Please enter date (yyyy-mm-dd)",
