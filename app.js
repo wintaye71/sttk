377dcce5047b5ecebb9287a202bed2f4ce8c86ae
wintaye71 <ayewint71@gmail.com>
2020-10-25 21:41:59 +0630

    pending

--- app.js
+++ app.js
@@ -537,7 +537,7 @@ app.get('/webview2/:sender_id', function (req, res) {
   res.render('registrationPendingWebview.ejs', { title: "Booking Update", updateData: updateData, sender_id: sender_id });
 });
 
-app.post('/webview2', upload.single('file'), function (req, res) {
+app.post('/webview2', function (req, res) {
   updateData = [];
   let name = req.body.name;
   let sender = req.body.sender_psid;
